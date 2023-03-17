2bca7c934526e98aee96518072cf9034dd1aa0ee
RSkooTy <127528782+RSkooTy@users.noreply.github.com>
2023-03-18 01:44:21 +0300

    Update main.cpp

--- Laba1/Laba1/main.cpp
+++ Laba1/Laba1/main.cpp
@@ -6,33 +6,46 @@
 using namespace std;
 
 
-float discriminant(float a, float b, float c)
+double Discriminant(double a, double b, double c)
 {
     return b*b - 4 * a * c;
 }
 
-void findQuadratic(float a, float b, float c)
+double firstDoubleRoots(double a, double b, double c)
 {
-    float x1 , x2, meaningDiscrim;
-    int prov = 0;
-    
-    meaningDiscrim = discriminant(a,b,c);
-    
-    if((a == prov) && (b == prov) && (c == prov))
+    double x1;
+    double disk = Discriminant(a, b, c);
+    x1 = (-b - sqrt(disk)/(2*a));
+    return x1;
+}
+
+double secondDoubleRoots(double a, double b, double c)
+{
+    double x2;
+    double disk = Discriminant(a, b, c);
+    x2 = (-b + sqrt(disk)/(2*a));
+    return x2;
+}
+
+double oneRoot(double a, double b, double c)
+{
+    double x;
+    x = -b/(2*a);
+    return x;
+}
+
+void findQuadratic(double a, double b, double c,ofstream& output)
+{
+    double searchDisc = Discriminant(a, b, c);
+    if(searchDisc > 0)
     {
-        cout << "Значение корней = 0" << endl;
-    }else if(meaningDiscrim > 0)
+        output << "два корня : " << firstDoubleRoots(a, b, c) << " "<< secondDoubleRoots(a, b, c);
+    }else if (searchDisc == 0)
     {
-        x1 = (-b + sqrt(meaningDiscrim))/(2 * a);
-        x2 = (-b - sqrt(meaningDiscrim))/(2 * a);
-        cout << "Значение x1: " << x1 << " Значение x2: " << x2 << endl;
-    }else if(meaningDiscrim == 0)
+        output << "единственный корень : " << oneRoot(a, b, c);
+    }else if (searchDisc < 0)
     {
-        x1 = -b/(2 * a);
-        cout << "Значение x: " << x1 << endl;
-    }else
-    {
-        cout << "Значение корней комплексные"<< endl;
+        output << "Корни комплексные";
     }
     
 }
@@ -42,20 +55,19 @@ void findQuadratic(float a, float b, float c)
 
 int main()
 {
-    float a , b , c;
     ifstream file("input.txt");
     ofstream file2("output.txt");
-    
     if(!file.is_open() || !file2.is_open())
     {
         cout << "error" << endl;
         return 1;
     }
-    
-    file >> a >> b >> c ;
-    
-    findQuadratic(a,b,c);
-    
+    double a,b,c;
+    while (file >> a >> b >> c)
+    {
+        findQuadratic(a, b, c, file2);
+    }
+
     file.close();
     file2.close();
     
